8abfa0beee964c63a5136d55c2492cec7d85271a
Tacio Zava <tacio.zava@hotmail.com>
2023-06-06 11:13:27 -0300

    feat: add logica php para autenticacao

--- registrar.php
+++ registrar.php
@@ -11,21 +11,43 @@ if ($conn->connect_error) {
     die("Falha na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-// Processamento do formulário
+// Autenticação do usuário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["nome"];
-    $tipoRegistro = $_POST["tipo_registro"];
+    $matricula = $_POST["matricula"];
+    $senha = $_POST["senha"];
 
-    // Inserir registro no banco de dados
-    $sql = "INSERT INTO registros (nome, tipo_registro) VALUES ('$nome', '$tipoRegistro')";
+    // Consultar o banco de dados para verificar as credenciais do usuário
+    $sql = "SELECT id, nome FROM usuarios WHERE matricula = '$matricula' AND senha = '$senha'";
+    $result = $conn->query($sql);
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Registro realizado com sucesso.";
+    if ($result && $result->num_rows > 0) {
+        // Autenticação bem-sucedida
+        $row = $result->fetch_assoc();
+        $nomeUsuario = $row["nome"];
+        $idUsuario = $row["id"];
+
+        // Verificar qual botão foi clicado
+        if (isset($_POST["tipo_registro"])) {
+            $tipoRegistro = $_POST["tipo_registro"];
+        }
+
+        // Processamento do formulário
+        if (!empty($tipoRegistro)) {
+            $nome = $_POST["nome"];
+
+            // Inserir registro no banco de dados
+            $sql = "INSERT INTO registros (id_usuario, nome, tipo_registro) VALUES ('$idUsuario', '$nome', '$tipoRegistro')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Registro realizado com sucesso. Bem-vindo, $nomeUsuario!";
+            } else {
+                echo "Erro ao registrar: " . $conn->error;
+            }
+        }
     } else {
-        echo "Erro ao registrar: " . $conn->error;
+        echo "Autenticação falhou. Verifique suas credenciais.";
     }
 }
 
 $conn->close();
 ?>
-
