34186c26089c13b60d6fbbaa77d2b9d69305f73a
Tacio Zava <tacio.zava@hotmail.com>
2023-06-06 13:07:54 -0300

    feat: verifica autenticacao e pagina para validar

--- registrar.php
+++ registrar.php
@@ -1,5 +1,17 @@
 <?php
-// Conexão com o banco de dados
+session_start();
+
+// Verificar se o usuário está autenticado
+if (!isset($_SESSION["autenticado"]) || $_SESSION["autenticado"] !== true) {
+    header("Location: login.html");
+    exit();
+}
+
+// Recuperar os dados do formulário
+$nome = $_POST["nome"];
+$tipoRegistro = $_POST["tipo_registro"];
+
+// Conectar-se ao banco de dados
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -11,43 +23,37 @@ if ($conn->connect_error) {
     die("Falha na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-// Autenticação do usuário
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $matricula = $_POST["matricula"];
-    $senha = $_POST["senha"];
+// Construir e executar a consulta de inserção
+$sql = "INSERT INTO registros (nome, tipo_registro) VALUES ('$nome', '$tipoRegistro')";
 
-    // Consultar o banco de dados para verificar as credenciais do usuário
-    $sql = "SELECT id, nome FROM usuarios WHERE matricula = '$matricula' AND senha = '$senha'";
-    $result = $conn->query($sql);
-
-    if ($result && $result->num_rows > 0) {
-        // Autenticação bem-sucedida
-        $row = $result->fetch_assoc();
-        $nomeUsuario = $row["nome"];
-        $idUsuario = $row["id"];
-
-        // Verificar qual botão foi clicado
-        if (isset($_POST["tipo_registro"])) {
-            $tipoRegistro = $_POST["tipo_registro"];
-        }
-
-        // Processamento do formulário
-        if (!empty($tipoRegistro)) {
-            $nome = $_POST["nome"];
-
-            // Inserir registro no banco de dados
-            $sql = "INSERT INTO registros (id_usuario, nome, tipo_registro) VALUES ('$idUsuario', '$nome', '$tipoRegistro')";
-
-            if ($conn->query($sql) === TRUE) {
-                echo "Registro realizado com sucesso. Bem-vindo, $nomeUsuario!";
-            } else {
-                echo "Erro ao registrar: " . $conn->error;
-            }
-        }
-    } else {
-        echo "Autenticação falhou. Verifique suas credenciais.";
-    }
+if ($conn->query($sql) === true) {
+    $mensagem = "Registro realizado com sucesso.";
+} else {
+    $mensagem = "Erro ao registrar o ponto: " . $conn->error;
 }
 
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Portal do Colaborador - Registro de Ponto</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+
+        h1 {
+            color: #333333;
+        }
+    </style>
+</head>
+<body>
+    <h1>Portal do Colaborador - Registro de Ponto</h1>
+    <p><?php echo $mensagem; ?></p>
+    <a href="registrar.html">Voltar</a>
+</body>
+</html>
